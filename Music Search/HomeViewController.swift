0ed13cf27dd20817a078ea6de92222cbbd1b6b55
Rafa Lopez <rlopez1715@gmail.com>
2016-11-02 00:39:42 -0600

    Added HomeViewController file and added a search bar to the home screen

---
+++ Music Search/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Music Search
+//
+//  Created by Rafael Lopez on 11/2/16.
+//  Copyright © 2016 Funkadelic. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
