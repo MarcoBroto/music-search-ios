9c7cb628ed1e1d05469c7ee74bb0be87214dac89
Rafa Lopez <rlopez1715@gmail.com>
2016-11-01 21:57:21 -0600

    Added search bar to home screen with a background, deleted Launch story board. created a HomePage swift file.

---
+++ Music Search/HomePageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomePageViewController.swift
+//  Music Search
+//
+//  Created by Rafael Lopez on 11/1/16.
+//  Copyright © 2016 Funkadelic. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
