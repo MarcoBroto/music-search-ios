0c3a5d52290837094ab18a5eff79631eb3e74048
MarcoBroto <msoto9940@gmail.com>
2016-11-01 23:57:30 -0600

    Branched from Marco's Branch

--- Music SearchTests/Music_SearchTests.swift
+++ Music SearchTests/Music_SearchTests.swift
@@ -28,7 +28,7 @@ class Music_SearchTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
+        self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
